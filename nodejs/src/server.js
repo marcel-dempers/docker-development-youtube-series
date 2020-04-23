ac3aed8fb9dcab66eb748b4bd5f0d1f95dae26d1
marcel-dempers <aimvec@gmail.com>
2020-04-24 09:16:53 +1000

    docker nodejs debugging

--- nodejs/src/server.js
+++ nodejs/src/server.js
@@ -2,15 +2,16 @@
 
 const express = require('express');
 
-// Constants
 const PORT = 5000;
 const HOST = '0.0.0.0';
 
-// App
 const app = express();
+
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+  res.send('Hello world \n');
 });
 
+
 app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+
+console.log(`Running version 3 on http://${HOST}:${PORT}`);
