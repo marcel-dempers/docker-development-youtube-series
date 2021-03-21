e183d4b9b841ff225f20b7a4e862a288408bbae9
marcel-dempers <aimvec@gmail.com>
2021-03-21 22:18:54 +1100

    tidy up

--- golang/introduction/part-4.commandline/videos/main.go
+++ golang/introduction/part-4.commandline/videos/main.go
@@ -1,51 +1,56 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "os"
+	"flag"
+	"fmt"
+	"os"
 )
 
 func main() {
+	
+	//'videos get' subcommand
+  getCmd := flag.NewFlagSet("get", flag.ExitOnError)
 
-		//'videos get' subcommand
-    getCmd := flag.NewFlagSet("get", flag.ExitOnError)
-    getAll := getCmd.Bool("all", false, "Get all videos")
-    getID := getCmd.String("id", "", "YouTube video ID")
+	// inputs for `videos get` command
+  getAll := getCmd.Bool("all", false, "Get all videos")
+  getID := getCmd.String("id", "", "YouTube video ID")
 
-		//'videos add' subcommand
-    addCmd := flag.NewFlagSet("add", flag.ExitOnError)
-    addID := addCmd.String("id", "", "YouTube video ID")
-		addTitle := addCmd.String("title", "", "YouTube video Title")
-		addUrl := addCmd.String("url", "", "YouTube video URL")
-		addImageUrl := addCmd.String("imageurl", "", "YouTube video Image URL")
-		addDesc := addCmd.String("desc", "", "YouTube video description")
-		
-    if len(os.Args) < 2 {
-        fmt.Println("expected 'get' or 'add' subcommands")
-        os.Exit(1)
-    }
+	//'videos add' subcommand
+	addCmd := flag.NewFlagSet("add", flag.ExitOnError)
 
-		//look at the 2nd argument's value
-		switch os.Args[1] {
-			case "get": // if its the 'get' command
-				HandleGet(getCmd, getAll, getID)
-			case "add": // if its the 'add' command
-				HandleAdd(addCmd, addID,addTitle,addUrl, addImageUrl, addDesc)
-			default: // if we don't understand the input
-				fmt.Println("expected 'get' or 'add' subcommands")
-        os.Exit(1)
-		}
+	// inputs for `videos add` command
+	addID := addCmd.String("id", "", "YouTube video ID")
+  addTitle := addCmd.String("title", "", "YouTube video Title")
+  addUrl := addCmd.String("url", "", "YouTube video URL")
+  addImageUrl := addCmd.String("imageurl", "", "YouTube video Image URL")
+  addDesc := addCmd.String("desc", "", "YouTube video description")
+
+	if len(os.Args) < 2 {
+		fmt.Println("expected 'get' or 'add' subcommands")
+		os.Exit(1)
+  }
+
+	//look at the 2nd argument's value
+	switch os.Args[1] {
+		case "get": // if its the 'get' command
+			HandleGet(getCmd, getAll, getID)
+		case "add": // if its the 'add' command
+			HandleAdd(addCmd, addID,addTitle,addUrl, addImageUrl, addDesc)
+		default: // if we don't understand the input
+	}
+
+
 }
 
 func HandleGet(getCmd *flag.FlagSet, all *bool, id *string){
+
 	getCmd.Parse(os.Args[2:])
 
 	if *all == false && *id == "" {
-		fmt.Print("id is required or specify --all for all videos")
-		getCmd.PrintDefaults()
-		os.Exit(1)
-	}
+    fmt.Print("id is required or specify --all for all videos")
+    getCmd.PrintDefaults()
+    os.Exit(1)
+  }
 
 	if *all {
 		//return all videos
@@ -66,16 +71,20 @@ func HandleGet(getCmd *flag.FlagSet, all *bool, id *string){
 			if id == video.Id {
 				fmt.Printf("ID \t Title \t URL \t ImageURL \t Description \n")
 				fmt.Printf("%v \t %v \t %v \t %v \t %v \n",video.Id, video.Title, video.Url, video.Imageurl,video.Description)
-
 			}
 		}
 	} 
+
+
+
 }
 
 func ValidateVideo(addCmd *flag.FlagSet,id *string, title *string, url *string, imageUrl *string, description *string ){
 
+	addCmd.Parse(os.Args[2:])
+
 	if *id == "" || *title == "" || *url == "" || *imageUrl == "" || *description == "" {
-		fmt.Println("all fields are required for adding a video")
+		fmt.Print("all fields are required for adding a video")
 		addCmd.PrintDefaults()
 		os.Exit(1)
 	}
@@ -83,7 +92,6 @@ func ValidateVideo(addCmd *flag.FlagSet,id *string, title *string, url *string, 
 }
 
 func HandleAdd(addCmd *flag.FlagSet,id *string, title *string, url *string, imageUrl *string, description *string ){
-	addCmd.Parse(os.Args[2:])
 
 	ValidateVideo(addCmd, id,title,url, imageUrl, description)
 
@@ -99,4 +107,5 @@ func HandleAdd(addCmd *flag.FlagSet,id *string, title *string, url *string, imag
 	videos = append(videos,video)
 
 	saveVideos(videos)
+
 }
--- golang/introduction/part-4.commandline/videos/videos.go
+++ golang/introduction/part-4.commandline/videos/videos.go
@@ -13,7 +13,6 @@ type video struct {
 	Url string 
 }
 
-
 func getVideos()(videos []video){
 	
 	fileBytes, err := ioutil.ReadFile("./videos.json")
