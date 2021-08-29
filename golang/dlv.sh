c2c0fb8c318bcec5b4fac38d53a07b8f57c4008b
marcel-dempers <aimvec@gmail.com>
2021-08-29 10:22:29 +1000

    go-debug-updates

--- golang/dlv.sh
+++ golang/dlv.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cd /go/src/work
-dlv debug --headless --log -l 0.0.0.0:2345 --api-version=2
+
+dlv --listen=:2345 --headless=true --log=true --log-output=debugger,debuglineerr,gdbwire,lldbout,rpc --accept-multiclient --api-version=2 exec /app
