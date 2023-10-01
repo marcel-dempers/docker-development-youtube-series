1ef3232ea03abbadc9ab165d59fea5cca6e3ad44
marcel-dempers <aimvec@gmail.com>
2023-10-01 18:27:03 +1100

    add new github actions updates

--- github/actions/self-hosted-runner/entrypoint.sh
+++ github/actions/self-hosted-runner/entrypoint.sh
@@ -2,14 +2,15 @@
 registration_url="https://api.github.com/repos/${GITHUB_OWNER}/${GITHUB_REPOSITORY}/actions/runners/registration-token"
 echo "Requesting registration URL at '${registration_url}'"
 
-payload=$(curl -sX POST -H "Authorization: token ${GITHUB_PAT}" ${registration_url})
+payload=$(curl -sX POST -H "Authorization: token ${GITHUB_PERSONAL_TOKEN}" ${registration_url})
 export RUNNER_TOKEN=$(echo $payload | jq .token --raw-output)
 
 ./config.sh \
     --name $(hostname) \
     --token ${RUNNER_TOKEN} \
+    -- labels my-runner \
     --url https://github.com/${GITHUB_OWNER}/${GITHUB_REPOSITORY} \
-    --work ${RUNNER_WORKDIR} \
+    --work "/work" \
     --unattended \
     --replace
 
