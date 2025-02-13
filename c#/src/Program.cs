9c289a54da3463aa3a74881a469b1002d644d68f
marcel-dempers <aimvec@gmail.com>
2025-02-13 18:05:12 +1100

    upgrade to dotnet 8

--- c#/src/Program.cs
+++ c#/src/Program.cs
@@ -1,25 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
-namespace work
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.Configure<HostOptions>(builder.Configuration.GetSection("HostOptions"));
+
+builder.WebHost.ConfigureKestrel(serverOptions =>
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
-        }
+    serverOptions.ListenAnyIP(5000);
+});
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseUrls("http://*:5000")
-                .UseStartup<Startup>();
-    }
-}
+var app = builder.Build();
+
+Console.WriteLine(Environment.ProcessorCount.ToString());
+
+app.UseHttpsRedirection();
+
+app.MapGet("/", async () =>
+{
+    await Task.Delay(40000); // Sleep for 40 seconds
+    return "Hello World!";
+});
+
+app.Run();
